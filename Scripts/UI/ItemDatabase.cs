31f6ab4b77554410b0f7f27c2022778817693343
Krelinos <emmer.palaming@gmail.com>
2023-08-12 17:03:57 -0500

    Successful JSON file reading for Items

--- Scripts/UI/ItemDatabase.cs
+++ Scripts/UI/ItemDatabase.cs
@@ -1,68 +1,113 @@
 using Godot;
 using System;
+using System.IO;
+using System.Text.Json;
+using System.Collections.Generic;
 
-public class ItemDatabaseManager
+namespace Moplestary
 {
-    Godot.Collections.Dictionary ItemDatabase;
-
-    public ItemDatabaseManager()
+    public class ItemDatabaseManager
     {
-        ItemDatabase = new Godot.Collections.Dictionary();
+        public HashSet< Item > ItemDatabase;
 
-        AddItem( new Godot.Collections.Dictionary{
-            {"name", "bruh"}
-            ,{"boowomp", "uwu"}
-            ,{"attackSpeed", 2.5f}
-        } );
+        public ItemDatabaseManager()
+        {
+            ItemDatabase = new HashSet< Item >();
 
-        foreach( var (key, value) in ItemDatabase )
-            GD.Print("K: " + key + " | V: " + value);
+            DeserializeBaseItems();
+
+            // ItemDatabase.Add( new Dictionary<String, Object>
+            // {
+            //     {"name", "bruh"}
+            //     ,{"boowomp", "uwu"}
+            //     ,{"attackSpeed", 2.5f}
+            // } );
+            // ItemDatabase.Add( new Dictionary<String, Object>
+            // {
+            //     {"firstname", "trans"}
+            //     ,{"slime", ":3"}
+            //     ,{"cooldown", 5f}
+            // } );
+
+            foreach( var value in ItemDatabase )
+            {
+                GD.Print( value );
+            
+                var j = JsonSerializer.Serialize( value );
+                GD.Print( "nyaaa" + j );
+#nullable enable
+                Item? k = JsonSerializer.Deserialize<Item>( j );
+                GD.Print( $"name: {k?.Name}" );
+            }
+        }
+
+        // https://docs.godotengine.org/en/stable/tutorials/io/saving_games.html
+        private void DeserializeBaseItems()
+        {
+            String filePath = Godot.ProjectSettings.GlobalizePath("res://Defs/Stuff.json");
+            GD.Print( filePath );
+
+            String fileText = File.ReadAllText( filePath );
+            GD.Print( fileText );
+
+            #nullable enable
+            JsonHelper? items = JsonSerializer.Deserialize< JsonHelper >( fileText );
+
+            if( items == null )
+            {
+                GD.Print("oof");
+                return;
+            }
+
+            GD.Print( items );
+
+            foreach( Item? item in items.ItemDefs )
+            {
+                GD.Print( item );
+                ItemDatabase.Add( item );
+            }
+
+            foreach( Item item in ItemDatabase )
+                GD.Print( item.Name );
+
+        }
+
+        class JsonHelper
+        {
+            public List< Item > ItemDefs { get; set; }
+        }
     }
 
-    bool AddItem( Godot.Collections.Dictionary itemDictionary )
+    #nullable enable
+    public class Item
     {
-        int itemHash = GD.Hash( itemDictionary );
+        public String Name { get; set; } = "Unnamed Item";
+        public String? SpritePath { get; set; } = "Debug/SpriteNotFound";
+        public String? FlavorText { get; set; }
+        public int? Rarity { get; set; }    // 0 Common | 1 Uncommon | 2 Rare | 3 Epic | 4 Legendary | 5 Unique
+        public int? MaxStack { get; set; }
 
-        if ( !ItemDatabase.ContainsKey(itemHash) )
-        {
-            ItemDatabase[itemHash] = itemDictionary;
-            return true;
-        }
-        else
-            return false;
+        // For Equipment-type items
+        public int? Type { get; set; }     // 0 Head | 1 Torso | 2 Legs | 3 Warrior weapon | 4 Pirate weapon | etc.
+
+        public int? MinDamage { get; set; }
+        public int? MaxDamage { get; set; }
+        public float? AttackSpeed { get; set; }
+
+        #nullable enable
+        public Dictionary<int, int>? StatModifiers { get; set; }
+        #nullable enable
+        public Dictionary<int, int>? SkillModifiers { get; set; }
+
+        // For Consumable-type items
+        public int? HPRestored { get; set; }
+        public int? MPRestored { get; set; }
+
+        public int? HPHalfLife { get; set; }
+        public int? MPHalfLife { get; set; }
+
+        public float? ConsumeTime { get; set; }
+
+        // List? sideEffects { get; set; }
     }
 }
-
-// class Item
-// {
-//     String name;
-//     String sprite;
-//     String flavor;
-//     int rarity; // 0 Common | 1 Uncommon | 2 Rare | 3 Epic | 4 Legendary | 5 Unique
-//     int maxStack;
-// }
-
-// class Equipment : Item
-// {
-//     int type;   // 0 Head | 1 Torso | 2 Legs | 3 Warrior | 4 Pirate | etc.
-
-//     int minDamage;
-//     int maxDamage;
-//     float attackSpeed;
-
-//     // Dictionary<int, int> statModifiers;
-//     // Dictionary<int, int> skillModifiers;
-// }
-
-// class Potion : Item
-// {
-//     int HPRestored;
-//     int MPRestored;
-
-//     int HPHalfLife;
-//     int MPHalfLife;
-
-//     float consumeTime;
-
-//     // List sideEffects;
-// }
--- Scripts/UI/ui_inventory.cs
+++ Scripts/UI/ui_inventory.cs
@@ -1,52 +1,55 @@
-using Godot;
-using System;
-using System.Collections.Generic;
+	using Godot;
+	using System;
+	using System.Collections.Generic;
 
-public partial class ui_inventory : Control
-{
-	ItemDatabaseManager ItemDB;
-
-	TileMap EquipTiles;
-	TileMap StorageTiles;
-
-	// Dictionary<Vector2I, Item> Equipment;
-	// Dictionary<Vector2I, Item> Inventory;
-
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
+	namespace Moplestary
 	{
-		EquipTiles = GetNode<TileMap>("EquipTiles");
-		StorageTiles = GetNode<TileMap>("StorageTiles");
-
-		// Equipment = new Dictionary<Vector2I, Item>();
-		// Inventory = new Dictionary<Vector2I, Item>();
-		ItemDB = new ItemDatabaseManager();
-	}
-
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	// public override void _Process(double delta)
-	// {
-	// }
-
-	public override void _GuiInput(InputEvent @event)
+	public partial class ui_inventory : Control
 	{
-		if (@event is InputEventMouseButton mb)
+		ItemDatabaseManager ItemDB;
+
+		TileMap EquipTiles;
+		TileMap StorageTiles;
+
+		// Dictionary<Vector2I, Item> Equipment;
+		// Dictionary<Vector2I, Item> Inventory;
+
+		// Called when the node enters the scene tree for the first time.
+		public override void _Ready()
 		{
-			if (mb.ButtonIndex == MouseButton.Left && mb.Pressed)
+			EquipTiles = GetNode<TileMap>("EquipTiles");
+			StorageTiles = GetNode<TileMap>("StorageTiles");
+
+			// Equipment = new Dictionary<Vector2I, Item>();
+			// Inventory = new Dictionary<Vector2I, Item>();
+			ItemDB = new ItemDatabaseManager();
+		}
+
+		// Called every frame. 'delta' is the elapsed time since the previous frame.
+		// public override void _Process(double delta)
+		// {
+		// }
+
+		public override void _GuiInput(InputEvent @event)
+		{
+			if (@event is InputEventMouseButton mb)
 			{
-			
-				// Vector2 mousePos = GetViewport().GetMousePosition();
-				Vector2 mousePos = mb.Position;
+				if (mb.ButtonIndex == MouseButton.Left && mb.Pressed)
+				{
+				
+					// Vector2 mousePos = GetViewport().GetMousePosition();
+					Vector2 mousePos = mb.Position;
 
-				Vector2 equTileLocalPos = mousePos - EquipTiles.Position;
-				Vector2 stoTileLocalPos = mousePos - StorageTiles.Position;
+					Vector2 equTileLocalPos = mousePos - EquipTiles.Position;
+					Vector2 stoTileLocalPos = mousePos - StorageTiles.Position;
 
-				Vector2I equTileMapPos = EquipTiles.LocalToMap( equTileLocalPos / EquipTiles.Scale );
-				Vector2I stoTileMapPos = StorageTiles.LocalToMap( stoTileLocalPos / StorageTiles.Scale );
+					Vector2I equTileMapPos = EquipTiles.LocalToMap( equTileLocalPos / EquipTiles.Scale );
+					Vector2I stoTileMapPos = StorageTiles.LocalToMap( stoTileLocalPos / StorageTiles.Scale );
 
-				GD.Print(Name + " has been clicked at " + mousePos);
-				GD.Print(EquipTiles.Name + " reports local pos of " + equTileLocalPos + " and map pos of " + equTileMapPos);
-				GD.Print(StorageTiles.Name + "reports local pos of " + stoTileLocalPos + " and map pos of " + stoTileMapPos);
+					GD.Print(Name + " has been clicked at " + mousePos);
+					GD.Print(EquipTiles.Name + " reports local pos of " + equTileLocalPos + " and map pos of " + equTileMapPos);
+					GD.Print(StorageTiles.Name + "reports local pos of " + stoTileLocalPos + " and map pos of " + stoTileMapPos);
+				}
 			}
 		}
 	}
